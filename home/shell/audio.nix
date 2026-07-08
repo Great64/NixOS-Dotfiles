df98dceadccbfa7093d5b8b9c51b87a690ff444a
Philip <philipozog@gmail.com>
2026-07-08 12:18:28 -0400

    updated audio see more stuff

--- home/shell/audio.nix
+++ home/shell/audio.nix
@@ -4,17 +4,61 @@
   home.packages = [
     (pkgs.writeShellApplication {
       name = "audio-switch";
-      runtimeInputs = [ pkgs.pulseaudio pkgs.gawk ];
+      runtimeInputs = with pkgs; [ pulseaudio gawk coreutils findutils rofi ];
       text = ''
-        chosen=$(pactl list sinks | awk '
-          /^[[:space:]]*Name:/        { name = $2 }
-          /^[[:space:]]*Description:/ { desc = substr($0, index($0,$2)); print desc "\t" name }
-        ' | rofi -dmenu -p "Audio Output" | awk -F'\t' '{print $2}')
+        CARD="alsa_card.pci-0000_01_00.1"
 
+        # Build tab-separated entries: label<TAB>type<TAB>value
+        entries=$(
+          # HDMI outputs as switchable profiles (NVIDIA card)
+          pactl list cards | awk '
+            /Name: alsa_card\.pci-0000_01_00\.1/ { in_card=1 }
+            in_card && /^    Profiles:/           { in_profiles=1; next }
+            in_card && in_profiles && /^        output:hdmi/ && /available: yes/ {
+              line = $0
+              gsub(/^[[:space:]]+/, "", line)
+              colon = index(line, ": ")
+              profile = substr(line, 1, colon - 1)
+              label   = substr(line, colon + 2)
+              sub(/ \(sinks:.*/, "", label)
+              print label "\tprofile\t" profile
+            }
+            in_card && in_profiles && /^    [^ ]/ { in_profiles=0 }
+            /^Card #/ && FNR > 1               { in_card=0 }
+          '
+
+          # Other sinks (headset, built-in, etc.)
+          pactl list sinks | awk '
+            /^[[:space:]]*Name:/        { name = $2 }
+            /^[[:space:]]*Description:/ {
+              desc = substr($0, index($0,$2))
+              if (name !~ /pci-0000_01_00\.1/) print desc "\tsink\t" name
+            }
+          '
+        )
+
+        chosen=$(printf '%s\n' "$entries" | awk -F'\t' '{print $1}' | rofi -dmenu -p "Audio Output")
         [ -z "$chosen" ] && exit 0
 
-        pactl set-default-sink "$chosen"
-        pactl list sink-inputs short | awk '{print $1}' | xargs -I{} pactl move-sink-input {} "$chosen"
+        entry=$(printf '%s\n' "$entries" | awk -F'\t' -v label="$chosen" '$1 == label {print; exit}')
+        type=$(printf '%s' "$entry"  | cut -f2)
+        value=$(printf '%s' "$entry" | cut -f3)
+
+        case "$type" in
+          profile)
+            pactl set-card-profile "$CARD" "$value"
+            sleep 0.3
+            new_sink=$(pactl list sinks short | awk '/pci-0000_01_00\.1/ {print $2; exit}')
+            if [ -n "$new_sink" ]; then
+              pactl set-default-sink "$new_sink"
+              pactl list sink-inputs short | awk '{print $1}' | xargs -I{} pactl move-sink-input {} "$new_sink"
+            fi
+            ;;
+          sink)
+            pactl set-default-sink "$value"
+            pactl list sink-inputs short | awk '{print $1}' | xargs -I{} pactl move-sink-input {} "$value"
+            ;;
+        esac
       '';
     })
   ];
