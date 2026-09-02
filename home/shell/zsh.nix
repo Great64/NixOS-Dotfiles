2364be37372c185b37f6e938fec9fc85165e0c3d
Philip <philipozog@gmail.com>
2026-09-02 14:30:00 -0400

    auto cd for update alias and unzip alias

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -8,7 +8,7 @@
     shellAliases = {
       ll = "ls -l";
       edit = "sudo -e";
-      update = "sudo nixos-rebuild switch --flake .#my-nixos";
+      update = "cd ~/my-nixos && sudo nixos-rebuild switch --flake .#my-nixos";
       test = "nix-shell -p";
       waydroid = "sudo systemctl start waydroid-container && waydroid session start & waydroid show-full-ui";
       vpnc = "protonvpn connect";
@@ -22,5 +22,10 @@
       ignoreAllDups = true;
       ignorePatterns = ["rm *" "pkill *" "cp *"];
     };
+    
+    # Custom p7zip alias
+    initContent = ''
+      unz() { 7z x "$1" -o"''${1%.*}"; }
+    '';
   };
 }
