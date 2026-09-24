5c25cbf7011b09c3268f9a427b7dcf73b391cb3e
Philip <philipozog@gmail.com>
2026-09-24 14:16:46 -0400

    fixed broken greeter

--- nixos/tuigreet.nix
+++ nixos/tuigreet.nix
@@ -1,25 +1,11 @@
 { pkgs, ... }:
 
 {
-  environment.etc."greetd/tuigreet.toml".text = ''
-    [[outputs]]
-    connector = "DP-2"
-    primary = true
-
-    [[outputs]]
-    connector = "DP-1"
-    enabled = false
-
-    [[outputs]]
-    connector = "DP-3"
-    enabled = false
-  '';
-
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --config /etc/greetd/tuigreet.toml --cmd niri-session";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd niri-session";
         user = "greeter";
       };
     };
